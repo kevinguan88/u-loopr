c7f3503ee4bde5623597511f1373d77b731ee278
kevinguan8868 <keving88@uw.edu>
2024-10-15 19:02:13 -0700

    implemented youtube package

--- src/app/page.js
+++ src/app/page.js
@@ -1,27 +1,22 @@
+"use client";
 import Image from "next/image";
+import YouTube from 'react-youtube';
 
 export default function Home() {
   return (
     <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
       <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
-        <Image
-          className="dark:invert"
-          src="https://nextjs.org/icons/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
+        <div className="text-xl font-[family-name:var(--font-geist-mono)]">
+          <h1 text-xl>U-Loopr</h1>
+        </div>
         <ol className="list-inside list-decimal text-sm text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
           <li className="mb-2">
-            Get started by editing{" "}
-            <code className="bg-black/[.05] dark:bg-white/[.06] px-1 py-0.5 rounded font-semibold">
-              src/app/page.js
-            </code>
-            .
+            Get started by pasting the link of the YouTube video you want to
+            loop.
           </li>
-          <li>Save and see your changes instantly.</li>
+          <li>Specify the section you want to loop.</li>
         </ol>
+        <YouTube videoId="DSBBEDAGOTc" />
 
         <div className="flex gap-4 items-center flex-col sm:flex-row">
           <a

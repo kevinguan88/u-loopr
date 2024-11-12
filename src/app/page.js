4d03631f50dbc9b9c36792bec236ee36e931a3f4
kevinguan8868 <keving88@uw.edu>
2024-11-11 17:46:43 -0800

    implemented video id search, started ui on slider

--- src/app/page.js
+++ src/app/page.js
@@ -4,13 +4,16 @@ import Image from "next/image";
 import YouTube from 'react-youtube';
 import { timeToSeconds } from './utils/timeToSeconds.js';
 import { secondsToTimeStamp } from './utils/secondsToTimeStamp.js';
+import ReactSlider from 'react-slider'
+import styled from 'styled-components';
 
 export default function Home() {
 
   const [startTime, setStartTime] = useState(0);
   const [endTime, setEndTime] = useState(0);
   const [currentTime, setCurrentTime] = useState(0);
-  const [duration, setDuration] = useState(0);
+  const [vidId, setVidId] = useState('DSBBEDAGOTc');
+  const linkInput = useRef(null);
   const playerRef = useRef(null);
   const intervalRef = useRef(null);
   const startInput = useRef(null);
@@ -106,8 +109,36 @@ export default function Home() {
     }
   }, [endInput])
 
+  const StyledSlider = styled(ReactSlider)`
+    width: 618px;
+    height: 7px;
+`;
+
+const StyledThumb = styled.div`
+    margin-top: -4px;
+    height: 17px;
+    line-height: 50px;
+    width: 17px;
+    text-align: center;
+    background-color: #f00;
+    color: #fff;
+    border-radius: 50%;
+    cursor: grab;
+`;
+
+const Thumb = (props, state) => <StyledThumb {...props}>{state.valueNow}</StyledThumb>;
+
+const StyledTrack = styled.div`
+    top: 0;
+    bottom: 0;
+    background: ${props => (props.index === 2 ? '#ddd' : props.index === 1 ? '#f00' : '#ddd')};
+    border-radius: 999px;
+`;
+
+const Track = (props, state) => <StyledTrack {...props} index={state.index} />;
+
   return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
+    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 font-[family-name:var(--font-geist-sans)]">
       <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
         <div className="text-xl font-[family-name:var(--font-geist-mono)]">
           <h1 text-xl>U-Loopr</h1>
@@ -119,14 +150,21 @@ export default function Home() {
           </li>
           <li>Specify the section you want to loop.</li>
         </ol>
+        <div className='flex '>
+          <input className='text-black px-2' ref={linkInput}></input>
+          <button className="relative inline-flex h-8 w-12 items-center justify-center rounded-md bg-neutral-950 transition-colors hover:bg-neutral-800 ms-2" onClick={() => {setVidId(linkInput.current.value)}}><div><svg width="15" height="15" viewBox="0 0 15 15" fill="none" xmlns="http://www.w3.org/2000/svg" class="h-5 w-5 text-neutral-200"><path d="M8.14645 3.14645C8.34171 2.95118 8.65829 2.95118 8.85355 3.14645L12.8536 7.14645C13.0488 7.34171 13.0488 7.65829 12.8536 7.85355L8.85355 11.8536C8.65829 12.0488 8.34171 12.0488 8.14645 11.8536C7.95118 11.6583 7.95118 11.3417 8.14645 11.1464L11.2929 8H2.5C2.22386 8 2 7.77614 2 7.5C2 7.22386 2.22386 7 2.5 7H11.2929L8.14645 3.85355C7.95118 3.65829 7.95118 3.34171 8.14645 3.14645Z" fill="currentColor" fill-rule="evenodd" clip-rule="evenodd"></path></svg></div></button>
+        </div>
         <YouTube
-          videoId="DSBBEDAGOTc"
+          videoId={vidId}
           onReady={(e) => {
             playerRef.current = e.target;
             resetLoop();
           }}
           onStateChange={handlePlayerStateChange}
         />
+        <div className='w-full flex justify-center'>
+          <StyledSlider defaultValue={[0, endTime]} max={endTime} renderTrack={Track} renderThumb={Thumb} />
+        </div>
         <div className='w-full flex justify-between'>
           <input onBlur={handleStartTimeChange} className='text-black text-center w-24' ref={startInput}/>
           <input onBlur={handleEndTimeChange} className='text-black text-center w-24' ref={endInput} />

4ae17ceeda21df33aad62a1c1625d83b871911fd
kevinguan8868 <keving88@uw.edu>
2024-11-23 22:34:26 -0800

    fixed slider functionality

--- src/app/page.js
+++ src/app/page.js
@@ -7,12 +7,35 @@ import { secondsToTimeStamp } from './utils/secondsToTimeStamp.js';
 import ReactSlider from 'react-slider'
 import styled from 'styled-components';
 
+const StyledSlider = styled(ReactSlider)`
+  width: 618px;
+  height: 7px;
+`;
+const StyledThumb = styled.div`
+  margin-top: -4px;
+  height: 17px;
+  line-height: 50px;
+  width: 17px;
+  text-align: center;
+  background-color: #f00;
+  color: #fff;
+  border-radius: 50%;
+  cursor: grab;
+`;
+const StyledTrack = styled.div`
+  top: 0;
+  bottom: 0;
+  background: ${props => (props.index === 2 ? '#ddd' : props.index === 1 ? '#f00' : '#ddd')};
+  border-radius: 999px;
+`;
+
 export default function Home() {
 
   const [startTime, setStartTime] = useState(0);
   const [endTime, setEndTime] = useState(0);
   const [currentTime, setCurrentTime] = useState(0);
   const [vidId, setVidId] = useState('DSBBEDAGOTc');
+  const [videoDuration, setVideoDuration] = useState(0);
   const linkInput = useRef(null);
   const playerRef = useRef(null);
   const intervalRef = useRef(null);
@@ -109,33 +132,29 @@ export default function Home() {
     }
   }, [endInput])
 
-  const StyledSlider = styled(ReactSlider)`
-    width: 618px;
-    height: 7px;
-`;
-
-const StyledThumb = styled.div`
-    margin-top: -4px;
-    height: 17px;
-    line-height: 50px;
-    width: 17px;
-    text-align: center;
-    background-color: #f00;
-    color: #fff;
-    border-radius: 50%;
-    cursor: grab;
-`;
-
-const Thumb = (props, state) => <StyledThumb {...props}>{state.valueNow}</StyledThumb>;
-
-const StyledTrack = styled.div`
-    top: 0;
-    bottom: 0;
-    background: ${props => (props.index === 2 ? '#ddd' : props.index === 1 ? '#f00' : '#ddd')};
-    border-radius: 999px;
-`;
+const Thumb = (props, state) => <StyledThumb {...props}></StyledThumb>;
 
 const Track = (props, state) => <StyledTrack {...props} index={state.index} />;
+
+const handleSliderChange = (value) => {
+  handleStartSliderChange(value[0]);
+  handleEndSliderChange(value[1]);
+};
+
+const handleStartSliderChange = (value) => {
+  const time = secondsToTimeStamp(value);
+  console.log('start timestamp is: ' + time);
+  startInput.current.value = time
+  setStartTime(value);
+};
+
+const handleEndSliderChange = (value) => {
+  const time = secondsToTimeStamp(value);
+  console.log('end timestamp is: ' + time);
+  endInput.current.value = time
+  setEndTime(value);
+};
+
 
   return (
     <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 font-[family-name:var(--font-geist-sans)]">
@@ -150,7 +169,7 @@ const Track = (props, state) => <StyledTrack {...props} index={state.index} />;
           </li>
           <li>Specify the section you want to loop.</li>
         </ol>
-        <div className='flex '>
+        <div className='flex w-40'>
           <input className='text-black px-2' ref={linkInput}></input>
           <button className="relative inline-flex h-8 w-12 items-center justify-center rounded-md bg-neutral-950 transition-colors hover:bg-neutral-800 ms-2" onClick={() => {setVidId(linkInput.current.value)}}><div><svg width="15" height="15" viewBox="0 0 15 15" fill="none" xmlns="http://www.w3.org/2000/svg" class="h-5 w-5 text-neutral-200"><path d="M8.14645 3.14645C8.34171 2.95118 8.65829 2.95118 8.85355 3.14645L12.8536 7.14645C13.0488 7.34171 13.0488 7.65829 12.8536 7.85355L8.85355 11.8536C8.65829 12.0488 8.34171 12.0488 8.14645 11.8536C7.95118 11.6583 7.95118 11.3417 8.14645 11.1464L11.2929 8H2.5C2.22386 8 2 7.77614 2 7.5C2 7.22386 2.22386 7 2.5 7H11.2929L8.14645 3.85355C7.95118 3.65829 7.95118 3.34171 8.14645 3.14645Z" fill="currentColor" fill-rule="evenodd" clip-rule="evenodd"></path></svg></div></button>
         </div>
@@ -158,12 +177,20 @@ const Track = (props, state) => <StyledTrack {...props} index={state.index} />;
           videoId={vidId}
           onReady={(e) => {
             playerRef.current = e.target;
+            setVideoDuration(playerRef.current.getDuration());
             resetLoop();
           }}
           onStateChange={handlePlayerStateChange}
         />
         <div className='w-full flex justify-center'>
-          <StyledSlider defaultValue={[0, endTime]} max={endTime} renderTrack={Track} renderThumb={Thumb} />
+          <StyledSlider 
+            value={[startTime, endTime]}
+            max={videoDuration} 
+            renderTrack={Track} 
+            renderThumb={Thumb} 
+            onChange={handleSliderChange}
+            step={1} 
+          />
         </div>
         <div className='w-full flex justify-between'>
           <input onBlur={handleStartTimeChange} className='text-black text-center w-24' ref={startInput}/>

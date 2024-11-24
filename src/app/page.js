100dc5e03cc2826c074f07333d3c42ffe4c071ef
kevinguan8868 <keving88@uw.edu>
2024-11-23 23:11:25 -0800

    styling changes

--- src/app/page.js
+++ src/app/page.js
@@ -164,7 +164,7 @@ const handleEndSliderChange = (value) => {
 
 
   return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 font-[family-name:var(--font-geist-sans)]">
+    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen pb-20 gap-10 font-[family-name:var(--font-geist-sans)]">
       <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
         <div className="text-xl font-[family-name:var(--font-geist-mono)]">
           <h1 text-xl>U-Loopr</h1>
@@ -176,8 +176,8 @@ const handleEndSliderChange = (value) => {
           </li>
           <li>Specify the section you want to loop.</li>
         </ol>
-        <div className='flex w-40'>
-          <input className='text-black px-2' ref={linkInput}></input>
+        <div className='flex w-96' style={{width: '28rem'}}>
+          <input className='text-black px-2 w-96 rounded' ref={linkInput} style={{width: '28rem'}} placeholder='Paste YouTube link here'></input>
           <button className="relative inline-flex h-8 w-12 items-center justify-center rounded-md bg-neutral-950 transition-colors hover:bg-neutral-800 ms-2" onClick={() => {getVideoId(linkInput.current.value)}}><div><svg width="15" height="15" viewBox="0 0 15 15" fill="none" xmlns="http://www.w3.org/2000/svg" class="h-5 w-5 text-neutral-200"><path d="M8.14645 3.14645C8.34171 2.95118 8.65829 2.95118 8.85355 3.14645L12.8536 7.14645C13.0488 7.34171 13.0488 7.65829 12.8536 7.85355L8.85355 11.8536C8.65829 12.0488 8.34171 12.0488 8.14645 11.8536C7.95118 11.6583 7.95118 11.3417 8.14645 11.1464L11.2929 8H2.5C2.22386 8 2 7.77614 2 7.5C2 7.22386 2.22386 7 2.5 7H11.2929L8.14645 3.85355C7.95118 3.65829 7.95118 3.34171 8.14645 3.14645Z" fill="currentColor" fill-rule="evenodd" clip-rule="evenodd"></path></svg></div></button>
         </div>
         <YouTube
@@ -200,8 +200,8 @@ const handleEndSliderChange = (value) => {
           />
         </div>
         <div className='w-full flex justify-between'>
-          <input onBlur={handleStartTimeChange} className='text-black text-center w-24' ref={startInput}/>
-          <input onBlur={handleEndTimeChange} className='text-black text-center w-24' ref={endInput} />
+          <input onBlur={handleStartTimeChange} className='text-black text-center w-24 rounded' ref={startInput}/>
+          <input onBlur={handleEndTimeChange} className='text-black text-center w-24 rounded' ref={endInput} />
         </div>
 
         <div className="flex gap-4 items-center flex-col sm:flex-row">
